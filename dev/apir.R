41467f0918a6d8cc3a23cf1b4add7ebb718eaf2c
Štěpán Mikula <stepan.mikula@gmail.com>
2022-01-04 21:31:40 +0100

    r file

--- dev/apir.R
+++ dev/apir.R
@@ -1,20 +1,18 @@
 library(tidyverse)
-
-library(reticulate)
 library(jsonlite)
 library(httr)
+library(lubridate)
+
+timeg <- Sys.time() - 165
 
 
 req <- POST("https://ke-ap01.econ.muni.cz/getData.php", 
           body = list(
-            name = "*",
-            time = "2021-12-25 11:00:00"
+            name = '*',
+            time = as.character(timeg)
           ),
           encode = "json")
 
 req$status_code
 content(req, as = "text")
-
 stop_for_status(req)
-json <- content(req, "text")
-json
